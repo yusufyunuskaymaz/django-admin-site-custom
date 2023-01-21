e7dd32dd262e09b1d96cfed1fb9acc11afb908ef
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-01-21 15:07:48 +0300

    admin

--- product/admin.py
+++ product/admin.py
@@ -6,12 +6,33 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ("is_in_stock",)
     list_display_links = ("create_date","name")
     list_filter = ("is_in_stock", "create_date",)
-    ordering = ("-update_date",)
-    search_fields = ("name",)
+    ordering = ("-is_in_stock",)
+    # search_fields = ("name",)
     prepopulated_fields = {"slug" : ("name",)}
     list_per_page = 15
-    date_hierarchy = "update_date"
-    fields = (("name", "slug"), "description", "is_in_stock")
+    # date_hierarchy = "update_date"
+    # fields = (("name", "slug"), "description", "is_in_stock")
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('name',"slug"), "is_in_stock" # to display multiple fields on the same line, wrap those fields in their own tuple
+            ),
+            # 'classes': ('wide', 'extrapretty'), wide or collapse
+        }),
+        ('My section', {
+            "classes" : ("collapse", ),
+            "fields" : ("description",),
+            'description' : "You can use this section for optionals settings"
+        })
+    )
+    
+    actions = ("is_in_stock", )
+    
+    def is_in_stock(self, request, queryset):
+        count = queryset.update(is_in_stock=True)
+        self.message_user(request, f"{count} çeşit ürün stoğa eklendi") 
+        
+    is_in_stock.short_description = "İşaretlenen ürünleri stoğa ekle"
 
 
 
