6ca58d3f7208a9cb59ce2be56adf16366e021877
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-01-21 14:45:21 +0300

    admin-panel customized

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_date", "is_in_stock", "update_date")
+    list_editable = ("is_in_stock",)
+    list_display_links = ("create_date","name")
+    list_filter = ("is_in_stock", "create_date",)
+    ordering = ("-update_date",)
+    search_fields = ("name",)
+    prepopulated_fields = {"slug" : ("name",)}
+    list_per_page = 15
+    date_hierarchy = "update_date"
+    fields = (("name", "slug"), "description", "is_in_stock")
+
+
+
+admin.site.register(Product, ProductAdmin)
 
 admin.site.site_title = "Back End Site"
 admin.site.site_header = "Deneme"
